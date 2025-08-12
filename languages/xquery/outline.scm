71d84991a40004b4db2836c47162ad45dc4e4667
wolfgangmm <wolfgangmm@exist-db.org>
2025-08-12 15:14:56 +0200

    Initial commit

---
+++ languages/xquery/outline.scm
@@ -0,0 +1,10 @@
+; Ultra-minimal XQuery outline test
+; Just testing if any nodes are detected at all
+
+(function_declaration
+    local: (identifier) @name
+    (param_list) @context
+) @item
+
+(variable_declaration) @item
+(namespace_declaration) @item
